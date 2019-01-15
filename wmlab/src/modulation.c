624b942b26daa1dac4800c115065b7a4e363ae9c
Paul Bertrand <bertrapa@ensipc517.ensimag.fr>
2019-01-15 15:48:55 +0100

    Francois la rageuse

--- wmlab/src/modulation.c
+++ wmlab/src/modulation.c
@@ -1,10 +1,20 @@
 #include <it/mat.h>
 
-vec encode_ss( vec x, bvec m, mat u, double wcr ) 
+vec encode_ss( vec x, bvec m, mat u, double wcr )
 {
-  vec w = NULL; 
+  vec w = vec_clone(x);
+  int i;
+  for (i=0; i<mat_width(u); i++) {
+    vec ui = mat_get_get(u,i);
+    if ( m[i]==0){
+      vec_add(w, ui);
+    }
+    else{
+      vec_sub(w, ui);
+    }
+    vec_delete(ui);
+  }
 
-  return( w ); 
+  return( w );
 
 }
-
