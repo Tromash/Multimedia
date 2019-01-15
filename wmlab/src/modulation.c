281f1b82bc93600be548f84acfcf7d4fe84f669c
Francois Gauthier <francois.gauthier@ensimag.grenoble-inp.fr>
2019-01-15 16:07:17 +0100

    toto

--- wmlab/src/modulation.c
+++ wmlab/src/modulation.c
@@ -5,7 +5,7 @@ vec encode_ss( vec x, bvec m, mat u, double wcr )
   vec w = vec_clone(x);
   int i;
   for (i=0; i<mat_width(u); i++) {
-    vec ui = mat_get_get(u,i);
+    vec ui = mat_get_col(u,i);
     if ( m[i]==0){
       vec_add(w, ui);
     }
